90c5dec45046876dde467c71303577e8b28f89cb
ahatrainings <79284039+ahatrainings@users.noreply.github.com>
2021-02-22 20:28:09 -0500

    added

---
+++ Sum.java
@@ -0,0 +1,60 @@
+public class Sum{
+
+
+public static void main(String args[]){
+
+
+/*int - stores the numeric value
+long - stores numeric value 
+
+
+float and double stores the float value eg 10.6
+
+boolean stores true or false
+
+char stores single character eg: 'c'  ' ' '5' 's' 'f'
+
+*/
+
+//assigned a numeric value in variable a of type int
+int a = 19;
+//over riding variable a with value 20
+a = 20;
+//
+boolean b = false;
+b = true;
+//
+char c = '5';
+c = '6';
+//create or assign
+String s = "100";
+//override
+System.out.println("printing value" + s);
+
+s = "200";
+System.out.println("printing value" + s);
+
+s = "500";
+System.out.println("printing value" + s);
+
+//this is for notes
+if(true){
+
+}else{
+
+}
+//if condition expects only boolean variable either true or false
+
+//Conditions
+
+if(b){
+System.out.println("printing true");
+}else{
+System.out.println("going in else");
+}
+
+
+}
+
+
+}
